721fb1f8633e908918e8df404e4e84fab087a3f5
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2024-09-13 23:09:46 -0300

    Adiciona lógica para o servidor rodar em diferentes portas

--- src/lib/server.go
+++ src/lib/server.go
@@ -1,9 +1,54 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"net"
+	"os"
+	"go-cliente-servidor/src/helpers"
 )
 
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	var hash string
+	fmt.Fscanf(conn, "%s\n", &hash)
+
+	directory := "/tmp/alguma_coisa"
+	found, err := helpers.FindHash(hash, directory)
+	if err != nil {
+		fmt.Fprintf(conn, "error: %v\n", err)
+		return
+	}
+
+	if found {
+		fmt.Fprintf(conn, "found\n")
+	} else {
+		fmt.Fprintf(conn, "not found\n")
+	}
+}
+
 func main() {
-    fmt.Println("Servidor rodando...")
+	if len(os.Args) != 2 {
+		fmt.Println("Uso correto: ./servidor <porta>")
+		os.Exit(1)
+	}
+	port := os.Args[1]
+
+	ln, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		fmt.Println("Erro ao iniciar o servidor:", err)
+		os.Exit(1)
+	}
+	defer ln.Close()
+
+	fmt.Printf("Servidor rodando na porta %s...\n", port)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Erro ao aceitar conexão:", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
 }

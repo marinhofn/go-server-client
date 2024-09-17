f9c3c951b8bccc3ca5bed661fbf60bb5d552a65c
120110597 JOSE MARINHO FALCAO NETO <jose.marinho.neto@lcc3-013.lcc.ufcg.edu.br>
2024-09-17 14:45:38 -0300

    mudancas em adamento

--- src/helpers/sum.go
+++ src/helpers/sum.go
@@ -1,24 +1,36 @@
 package helpers
 
 import (
-	"crypto/sha256"
-	"fmt"
 	"io"
 	"os"
 )
 
-func Sum(filePath string) (string, error) {
+func Sum(filePath string) (int, error) {
 	file, err := os.Open(filePath)
 	if err != nil {
-		return "", err
+		return 0, err
 	}
 	defer file.Close()
 
-	hash := sha256.New()
-	if _, err := io.Copy(hash, file); err != nil {
-		return "", err
+	sum := 0
+	buf := make([]byte, 1024) // Lê o arquivo em blocos de 1024 bytes
+
+	for {
+		n, err := file.Read(buf)
+		if err != nil && err != io.EOF {
+			return 0, err
+		}
+		if n == 0 {
+			break
+		}
+
+		// Processa os bytes lidos
+		for _, b := range buf[:n] {
+			if b >= '0' && b <= '9' { // Verifica se é um dígito
+				sum += int(b - '0') // Converte o byte para o número e soma
+			}
+		}
 	}
 
-	return fmt.Sprintf("%x", hash.Sum(nil)), nil
+	return sum, nil
 }
-
